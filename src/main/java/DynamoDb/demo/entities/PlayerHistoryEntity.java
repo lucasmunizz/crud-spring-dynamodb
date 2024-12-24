83c312013cbc0dcca888bf0366da56ae340eb916
Lucas Muniz <lucaslima1048@gmail.com>
2024-12-24 14:13:53 -0300

    create entity

---
+++ src/main/java/DynamoDb/demo/entities/PlayerHistoryEntity.java
@@ -0,0 +1,59 @@
+package DynamoDb.demo.entities;
+
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSortKey;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@DynamoDbBean
+public class PlayerHistoryEntity {
+
+    private String username;
+
+    private UUID gameId;
+
+    private Double score;
+
+    private Instant created_at;
+
+    @DynamoDbAttribute("username")
+    @DynamoDbPartitionKey
+    public String getUsername(){
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @DynamoDbAttribute("game_id")
+    @DynamoDbSortKey
+    public UUID getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(UUID gameId) {
+        this.gameId = gameId;
+    }
+
+    @DynamoDbAttribute("score")
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    @DynamoDbAttribute("created_at")
+    public Instant getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Instant created_at) {
+        this.created_at = created_at;
+    }
+}
