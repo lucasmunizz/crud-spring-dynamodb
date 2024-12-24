d59dec64a77882905567bdc3a730ba6b6e43598d
Lucas Muniz <lucaslima1048@gmail.com>
2024-12-24 14:03:48 -0300

    first-commit

---
+++ src/main/java/DynamoDb/demo/DynamoDbProjectApplication.java
@@ -0,0 +1,13 @@
+package DynamoDb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamoDbProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamoDbProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DynamoDb/demo/DynamoDbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package DynamoDb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamoDbProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
